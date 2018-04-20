66c6334e0f4fd2327abb8b67585791ec2f1b5e9d
Unknown <wlhumphreys@student.rtc.edu>
2018-04-19 19:12:46 -0700

    //MainCode

---
+++ pulllab.py
@@ -0,0 +1,11 @@
+def lone_sum(a, b, c):
+  if(a >= b):
+    return c
+  elif(a == c):
+    return b
+  elif(b == c):
+    return a
+  elif(a == b and a == c and b == c):
+    return 0
+  else:
+    return a+b+c
